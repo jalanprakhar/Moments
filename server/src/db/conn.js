2dbe26ad3eb8de4aff9daf589545460e4343385a
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-12 01:17:04 +0530

    switched to atlas and added .env files for security

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const mongoose=require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/moments').then(()=>{
-    console.log(`Connection Successfull`)
-}).catch((e)=>console.log(e));
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.71yhk.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log(`Connection Successfull`);
+  })
+  .catch((e) => console.log(e));
