3db97a7c6f5cb07f34f1ed5ee4c88f74ef2a3289
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-16 02:23:50 +0530

    removing errors

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,56 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-import moments from '../../images/moments.png'
-import useStyles from './styles';
-import {Link,useLocation} from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
+import moments from "../../images/moments.png";
+import useStyles from "./styles";
+import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Navbar() {
-    const location=useLocation();
-    const[user,setUser]=useState(JSON.parse(localStorage.getItem("profile")))
-    const navigate=useNavigate();
-    // const user=null;
-    const classes = useStyles();
-    const logout=()=>{
-        setUser(null);
-        localStorage.clear();
-        navigate('/auth')
+  const location = useLocation();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const navigate = useNavigate();
+  // const user=null;
+  const classes = useStyles();
+  const logout = () => {
+    setUser(null);
+    localStorage.clear();
+    navigate("/auth");
+  };
+  useEffect(() => {
+    if (user) {
+      // const token=user.token
     }
-    useEffect(()=>{
-        if(user){
-
-        const token=user.token
-        }
-        setUser(JSON.parse(localStorage.getItem("profile")))
-    },[location])
-    return (
-        <AppBar className={`${classes.appBar}`} position='static' color='inherit'>
-            <div className={classes.brandContainer}>
-                <Typography className={classes.heading} variant='h2' align='center' component={Link} to='/'>
-                    Moments
-                </Typography>
-                <img className={classes.image} src={moments} alt='moments' height='60' />
-            </div>
-            <Toolbar className={classes.toolbar}>
-                {user?(
-                    <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
-                            {user.result.name.charAt(0)}
-                        </Avatar>
-                        <Typography className={classes.userName} variant='h6'>
-                            {user.result.name.split(" ")[0]}
-                        </Typography>
-                        <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>
-                            Logout
-                        </Button>
-                    </div>
-                ):(
-                    <Button component={Link} to='/auth' variant='contained' color='primary'> 
-                        Sign In
-                    </Button>
-                )}
-            </Toolbar>
-        </AppBar>
-    )
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+  return (
+    <AppBar className={`${classes.appBar}`} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography
+          className={classes.heading}
+          variant="h2"
+          align="center"
+          component={Link}
+          to="/"
+        >
+          Moments
+        </Typography>
+        <img
+          className={classes.image}
+          src={moments}
+          alt="moments"
+          height="60"
+        />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name.split(" ")[0]}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,55 +1,82 @@
-import React from 'react'
-import useStyles from './styles';
-import { Card, CardActions, CardMedia, Button, Typography, CardContent } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import DeleteIcon from '@material-ui/icons/Delete';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import moment from 'moment';
+import React from "react";
+import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardMedia,
+  Button,
+  Typography,
+  CardContent,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import { deletePost} from '../../../actions/posts';
-const Post = ({ post,setCurrentId }) => {
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
+
+import { deletePost } from "../../../actions/posts";
+const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
-  const dispatch=useDispatch();
-  const user=JSON.parse(localStorage.getItem("profile"));
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
   return (
- 
-      <Card className={classes.card}>
-        <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
-        <div className={classes.overlay}>
-          <Typography variant='h6'>
-            {post.name}
-          </Typography>
-          <Typography variant='body2'>
-            {moment(post.createdAt).fromNow()}
-          </Typography>
-
-        </div>
-        {user && user.result._id===post.creator &&<div className={classes.overlay2}>
-          <Button style={{ color: 'white' }} size='small' onClick={() => { setCurrentId(post._id)}}>
-            <MoreHorizIcon fontSize='medium' />
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.name}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      {user && user.result._id === post.creator && (
+        <div className={classes.overlay2}>
+          <Button
+            style={{ color: "white" }}
+            size="small"
+            onClick={() => {
+              setCurrentId(post._id);
+            }}
+          >
+            <MoreHorizIcon fontSize="medium" />
           </Button>
-        </div>}
-        <div className={classes.details}>
-          <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
         </div>
-        <Typography className={classes.title} variant='h5' gutterBottom>
-            {post.title}
-          </Typography>
-        <CardContent>
-          <Typography  variant='body2' color='textSecondary' component='p' gutterBottom>
-            {post.message}
-          </Typography>
-        </CardContent>
-        <CardActions className={classes.cardActions}>
-          
-          {user && user.result._id===post.creator && <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
-            <DeleteIcon fontSize='small' />
+      )}
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
+      </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          gutterBottom
+        >
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        {user && user.result._id === post.creator && (
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => dispatch(deletePost(post._id))}
+          >
+            <DeleteIcon fontSize="small" />
             Delete
-          </Button>}
-        </CardActions>
-      </Card>
-  )
-}
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
+};
 
-export default Post
+export default Post;
