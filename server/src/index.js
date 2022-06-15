c73029013ad5511f521c298d21897e4fe7de5ef0
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-16 03:11:28 +0530

    ,

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,17 @@
-
-const express=require('express');
-require('./db/conn')
-const cors=require('cors');
-const server=express();
-const port=process.env.PORT || 8000;
-const postRouter=require('./routes/posts.js');
-const userRouter=require('./routes/user.js');
-server.use(express.json(
-    {limit:'200mb'}
-));
+const express = require("express");
+require("./db/conn");
+const cors = require("cors");
+const server = express();
+const port = process.env.PORT || 8000;
+const postRouter = require("./routes/posts.js");
+const userRouter = require("./routes/user.js");
+server.use(express.json({ limit: "200mb" }));
 server.use(cors());
 
+server.use("/posts", postRouter);
 
+server.use("/user", userRouter);
 
-
-server.use('/posts',postRouter);
-
-server.use('/user',userRouter);
-
-
-server.listen(port,()=>{
-    console.log('Listening');
-})
+server.listen(port, () => {
+  console.log("Listening");
+});
